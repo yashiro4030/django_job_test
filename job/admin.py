cc3fa8d3c2578618758e80dc0fdea1523e3535b8
yashiro4030 <charai.driss.pro@gmail.com>
2021-09-05 01:33:26 +0200

    L'ajout models job et category

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db.models.fields import json
+
+from .models import Category, Job
+
+
+admin.site.register(Job)
+admin.site.register(Category)
 
 # Register your models here.
--- job/models.py
+++ job/models.py
@@ -1,3 +1,26 @@
+from typing import Tuple
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    cat_title = models.CharField(max_length=100,null=True)
+    publish_at=models.DateField(null=True,auto_now=True)
+
+    def __str__(self) -> str:
+        return self.cat_title
+
+class Job(models.Model):
+    JOB_TYPE=(("FULL TIME","FULL TIME"),("PART TIME","PART TIME"))
+    title = models.CharField(max_length=100,null=True)
+    descript=models.TextField(max_length=500,null=True)
+    location = models.CharField(max_length=100,null=True)
+    job_type=  models.CharField(max_length=100,choices=JOB_TYPE)
+    publish_at=models.DateField(null=True,auto_now=True)
+    vacancy = models.IntegerField(null=True)
+    salary = models.FloatField(null=True)
+    experience = models.IntegerField(null=True)
+    Category = models.ForeignKey('category',null=True,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
+
+    
