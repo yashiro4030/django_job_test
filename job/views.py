57e2e86b4f75236135d782c79cdf9b1f8138a611
yashiro4030 <charai.driss.pro@gmail.com>
2021-09-06 23:21:16 +0200

    creation urls job

--- job/views.py
+++ job/views.py
@@ -1,3 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def job_list(request):
+    return render(request,"")
+
+
+
+def job_detail(request,id):
+    return render(request,"")
